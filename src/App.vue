1f03748040e84f86cf83bab8664de20dbc84dd8a
John Pham <johnphammail@gmail.com>
2017-12-11 18:28:26 -0800

    init with layout

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home'
+
+export default {
+  name: 'ACM-Billboard',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+html, body {
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+}
+
+div {
+  height: inherit;
+  width: inherit;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <iframe class="w100 h33" src="http://bcoelc.com" frameborder="0"></iframe>
+    <iframe class="w100 h33" src="http://johnpham.net/calendar" frameborder="0"></iframe>
+    <iframe class="w50 h33" src="http://johnpham.net/calendar" frameborder="0"></iframe>
+    <div class="w50 h33 is-right announcements">
+      <div class="w100 announcements-content">
+        <h1 class="announcements-header">{{ announcement.header }}</h1>
+        <p>{{ announcement.body }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      announcement: {
+        header: 'Winter 2018',
+        body: 'Welcome back yall!'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.w100 {
+  width: 100%;
+}
+
+.w50 {
+  width: 50%;
+}
+.h33 {
+  height: 33%;
+}
+.is-right {
+  float: right;
+}
+.block {
+  background-color: red;
+}
+.announcements-content {
+  text-align: center;
+}
+</style>
