bdd3d53d3272fbfc42e93fb1b7da51f38e093fe6
John Pham <johnphammail@gmail.com>
2017-12-12 11:06:45 -0800

    adding apollo client Adding ACM calendars Cropping calendar iframe

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,41 @@
 <template>
   <div>
-    <iframe class="w100 h33" src="http://bcoelc.com" frameborder="0"></iframe>
-    <iframe class="w100 h33" src="http://johnpham.net/calendar" frameborder="0"></iframe>
-    <iframe class="w50 h33" src="http://johnpham.net/calendar" frameborder="0"></iframe>
+    <iframe class="w100 h33"
+      src="http://bcoelc.com"
+      frameborder="0"
+      scrolling="no"
+    ></iframe>
+
+    <div class="w100 h33 is-cropped">
+      <iframe class="w100"
+        style="
+          height: 109%;
+        "
+        src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=o5etp9bnv2icv4kam97nn65n54%40group.calendar.google.com&amp;color=%23AB8B00&amp;ctz=America%2FLos_Angeles"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+
+    <div class="w50 h33 is-left is-cropped">
+      <iframe class="w100"
+        style="height: 109%;
+          zoom: 1.5; -moz-transform: scale(1.25); -moz-transform-origin: 0 0;"
+        src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=acmucr.org_gqk7rog6vidospeb1b9orug5t0%40group.calendar.google.com&amp;color=%23711616&amp;ctz=America%2FLos_Angeles" 
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+
     <div class="w50 h33 is-right announcements">
       <div class="w100 announcements-content">
-        <h1 class="announcements-header">{{ announcement.header }}</h1>
-        <p>{{ announcement.body }}</p>
+        <div v-if="!announcement.imageUrl">
+          <h1 class="announcements-header">{{ announcement.header }}</h1>
+          <p>{{ announcement.body }}</p>
+        </div>
+        <div v-else>
+          <img class="w100" :src="announcement.imageUrl"/>
+        </div>
       </div>
     </div>
   </div>
@@ -18,7 +47,8 @@ export default {
     return {
       announcement: {
         header: 'Winter 2018',
-        body: 'Welcome back yall!'
+        body: 'Welcome back yall!',
+        imageUrl: 'https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg'
       }
     }
   }
@@ -26,21 +56,26 @@ export default {
 </script>
 
 <style scoped>
+iframe {
+  overflow-y: hidden;
+}
+.is-cropped {
+  overflow: hidden;
+}
 .w100 {
   width: 100%;
 }
-
 .w50 {
   width: 50%;
 }
 .h33 {
   height: 33%;
 }
+.is-left {
+  float: left;
+}
 .is-right {
   float: right;
-}
-.block {
-  background-color: red;
 }
 .announcements-content {
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -2,12 +2,28 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import { ApolloClient, createNetworkInterface } from 'apollo-client'
+import VueApollo from 'vue-apollo'
+
+/* eslint-disable */
+const apolloClient = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://localhost:8080/'
+  }),
+  connectToDevTools: true
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
+  apolloProvider,
   template: '<App/>',
   components: { App }
 })
